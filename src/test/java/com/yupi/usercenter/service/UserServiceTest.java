be9605550390c8e4fc53a82fd8c5f480cabfc800
zhouqingkai <1208678644@qq.com>
2024-09-29 15:15:36 +0800

    ### 新增功能 - 在`application.yml`配置文件中添加了`mybatis-plus`配置，以关闭下划线到驼峰命名法的映射。

---
+++ src/test/java/com/yupi/usercenter/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.yupi.usercenter.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.yupi.usercenter.model.domain.User;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** 用户测试 */
+@Log4j2
+@SpringBootTest
+class UserServiceTest {
+
+  @Autowired private UserService userService;
+
+  @Test
+  void testAddUser() {
+    User user = new User();
+    user.setUsername("testyupi");
+    user.setUserAccount("123");
+    user.setAvatarUrl(
+        "https://cn.bing.com/images/search?q=%E5%9B%BE%E7%89%87&FORM=IQFRBA&id=D66BA18EE154D133745ABEF43F0A4AE1418ADA84");
+    user.setGender(0);
+    user.setUserPassword("123456");
+    user.setPhone("123");
+    user.setEmail("123");
+    boolean result = userService.save(user);
+    log.info("save user success, userId: " + user.getId());
+    assertTrue(result);
+  }
+}

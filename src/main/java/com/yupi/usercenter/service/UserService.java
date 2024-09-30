37e16558922a586a1ce4f66fa350db13861b896a
zhouqingkai <1208678644@qq.com>
2024-09-30 11:58:43 +0800

    集成用户登录功能并优化用户实体
    - 在 User 实体类中添加逻辑删除字段
    - 新增用户登录方法，实现用户登录功能
    - 优化用户注册方法，提高代码复用性
    - 实现用户信息脱敏方法
    - 配置全局逻辑删除字段

--- src/main/java/com/yupi/usercenter/service/UserService.java
+++ src/main/java/com/yupi/usercenter/service/UserService.java
@@ -1,14 +1,17 @@
 package com.yupi.usercenter.service;
 
-import com.yupi.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.usercenter.model.domain.User;
+import javax.servlet.http.HttpServletRequest;
 
 /**
-* @author 12086
-* @description 针对表【user(用户表)】的数据库操作Service
-* @createDate 2024-09-29 11:19:49
-*/
+ * @author 12086
+ * @description 针对表【user(用户表)】的数据库操作Service
+ * @createDate 2024-09-29 11:19:49
+ */
 public interface UserService extends IService<User> {
 
-    long userRegister(String userAccount, String userPassword, String checkPassword);
+  long userRegister(String userAccount, String userPassword, String checkPassword);
+
+  User userLogin(String userAccount, String userPassword, HttpServletRequest request);
 }
--- src/main/java/com/yupi/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/yupi/usercenter/service/impl/UserServiceImpl.java
@@ -1,23 +1,27 @@
 package com.yupi.usercenter.service.impl;
 
+import static com.sun.javafx.font.FontResource.SALT;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.yupi.usercenter.model.domain.User;
 import com.yupi.usercenter.mapper.UserMapper;
+import com.yupi.usercenter.model.domain.User;
 import com.yupi.usercenter.service.UserService;
+import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
-import static com.sun.javafx.font.FontResource.SALT;
-
 /**
- * @author 12086
+ * @author dadaguai
  * @description 针对表【user(用户表)】的数据库操作Service实现
  * @createDate 2024-09-29 11:19:49
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+  private static final String USER_INFO_STATE = "USER_INFO_STATE";
 
   /**
    * @param userAccount
@@ -57,9 +61,57 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     // 插入数据
     User user = User.builder().userAccount(userAccount).userPassword(encryptPassword).build();
     boolean saveResult = this.save(user);
-    if (!saveResult){
+    if (!saveResult) {
       return -1;
     }
     return user.getId();
+  }
+
+  /**
+   * @param userAccount
+   * @param userPassword
+   * @param request
+   * @return
+   */
+  public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+    // 校验
+    if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+      return null;
+    }
+    int len = userAccount.length();
+    if (len < 4) {
+      return null;
+    }
+    if (userPassword.length() < 8) {
+      return null;
+    }
+    // 校验账号是否含有不合法字符
+    String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+    if (userAccount.matches(validPattern)) {
+      return null;
+    }
+    String Md5Password = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+    // 查询
+    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+    queryWrapper.eq("userAccount", userAccount);
+    queryWrapper.eq("userPassword", Md5Password);
+    User dbUser = this.getOne(queryWrapper);
+    if (dbUser == null) {
+      return null;
+    }
+    // 脱敏
+    dbUser = getSafetyUser(dbUser);
+    request.getSession().setAttribute(USER_INFO_STATE, dbUser);
+    return dbUser;
+  }
+
+  private User getSafetyUser(User dbUser) {
+    User safeUser = new User();
+    BeanUtils.copyProperties(dbUser, safeUser);
+    safeUser.setUserStatus(null);
+    safeUser.setCreateTime(null);
+    safeUser.setIsDelete(null);
+    safeUser.setUpdateTime(null);
+    return safeUser;
   }
 }
